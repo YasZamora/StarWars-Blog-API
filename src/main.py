6b25e9002a7d58f0fb442366e5a4a9266db36197
YasZamora <yzamo001@gmail.com>
2022-03-10 15:51:10 +0000

    mi tarea

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Usuario, Personaje, Planeta, tabla_pivote1, tabla_pivote2
+import json
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +31,137 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/usuario', methods=['GET'])
+def handle_usuario():
+    obtenerUsuarios = Usuario.query.all()
+    lista = []
+    for user in obtenerUsuarios:
+        lista.append(user.serialize())
+    return jsonify(lista), 200    
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    # print(request.method)   # obtener metodo: GET POST, DELETE, PATCH, PUT
+    # print(request.get_json())   # obtener JSON enviado por cliente
+    # lista = []
+    # for usuario in usuarios:
+    #     lista.append(usuario.serialize())
 
-    return jsonify(response_body), 200
+    # return jsonify(lista), 200
+  
+
+@app.route('/personajes', methods=['GET'])
+def obtenerPersonajes():
+    personajes = Personaje.query.all()
+    listado = []   
+    for pers in personajes:
+        listado.append(pers.serialize())
+    return jsonify(listado), 200
+
+@app.route('/personajes/<int:personaje_id>', methods=['GET'])
+def obtenerPersonajes_id(personaje_id):
+    
+    # personaje = Personaje(name='yasna', birth_year="1988", eye_color='brown')
+    # db.session.add(personaje)
+    # db.session.commit()
+    personajes = Personaje.query.filter_by(id=personaje_id)[0]
+    print('_'*80)
+    print(Personaje.query.filter_by(id=personaje_id)[0])
+    print('_'*80)
+    return jsonify(personajes.serialize()), 200   
+
+@app.route('/planetas', methods=['GET'])
+def obtenerPlanetas():
+    planetas = Planeta.query.all()
+    lista2 = []
+    for planets in planetas:
+        lista2.append(planets.serialize())
+    return jsonify(lista2), 200   
+    
+@app.route('/planetas/<int:planeta_id>', methods=['GET'])
+def obtenerPlaneta_id(planeta_id):
+    
+    # personaje = Personaje(name='yasna', birth_year="1988", eye_color='brown')
+    # db.session.add(personaje)
+    # db.session.commit()
+    planetas = Planeta.query.filter_by(id=planeta_id).first()
+    return jsonify(planetas.serialize()), 200                 
+
+@app.route('/usuario/favoritos/<int:usuario_id>', methods=['GET'])
+def usuarioFavoritos(usuario_id):
+    print('-'*80)
+    # El siguiente código se hace porque no se tienen usuarios, entonces se crean a la mala, esta forma se usa para agregar registros en la base de datos, instanciando la clase, agregandola en la BD y luego guardándola en la base de datos: 
+    # usuario = Usuario(name='Yas3',contrasena='ajdkslf')
+    # db.session.add(usuario)
+    # db.session.commit()
+    # print(Usuario.query.filter_by(id=2).first().personaje)
+    # print('UsuarioPersonajes:', Usuario.query.filter_by(id=2).first().personaje)
+    usuarioPersonajes = Usuario.query.filter_by(id=usuario_id).first().personajes
+    print(usuarioPersonajes)
+    # usuario = Usuario.query.filter_by(id=2).first()
+    # personaje_favorito = Personaje.query.filter_by(id=1).first()
+    # usuario.personaje.append(personaje_favorito)
+    # db.session.add(usuario)
+    # db.session.commit()
+  
+    print('-'*80)
+    # print('UsuarioPlanetas:', Usuario.query.filter_by(id=3).first().planeta)
+    usuarioPlanetas = Usuario.query.filter_by(id=usuario_id).first().planetas
+    # usuario = Usuario.query.filter_by(id=usuario_id).first()
+    # planeta_favorito = Planeta.query.filter_by(id=2).first()
+    # usuario.planeta.append(planeta_favorito)
+    # db.session.add(usuario)
+    # db.session.commit()
+
+    print('-'*90)
+    lista = []
+    for personaje_favorito in usuarioPersonajes : 
+        lista.append(personaje_favorito.serialize())
+    for planeta_favorito in usuarioPlanetas :
+        lista.append(planeta_favorito.serialize())    
+    return jsonify(lista), 200
+
+@app.route('/favoritos/planetas/<int:planeta_id>' , methods= ['POST'])
+def favoritos_planetas_create(planeta_id):
+    planeta = Planeta.query.get(planeta_id)
+    usuario = Usuario.query.get(1)
+    usuario.planetas.append(planeta)
+    db.session.commit()
+    return jsonify({"succses": "Ya se agregó su planeta favorito satisfactoriamente"}), 201
+
+@app.route('/favoritos/personajes/<int:personaje_id>' , methods= ['POST'])    
+def favoritos_personajes_create(personaje_id):
+    personaje = Personaje.query.get(personaje_id)
+    usuario = Usuario.query.get(1)
+    usuario.personajes.append(personaje)
+    db.session.commit()
+    return jsonify({"succes": "Ya se agregó su personaje favorito satisfactoriamente"}), 201
+
+@app.route('/favoritos/planetas/<int:planeta_id>' , methods=['DELETE'])   
+def favoritos_planetas_delete(planeta_id):
+    planeta = Planeta.query.get(planeta_id)
+    usuario = Usuario.query.get(1)
+    usuario.planetas.remove(planeta)
+    db.session.commit()
+    return jsonify({"succes": "Ya se elimino el planeta favorito satisfactoriamente"}), 200
+
+@app.route('/favoritos/personajes/<int:personaje_id>' , methods = ['DELETE'])  
+def favoritos_personajes_delete(personaje_id):
+    personaje = Personaje.query.get(personaje_id)
+    usuario = Usuario.query.get(1)
+    usuario.personajes.remove(personaje)
+    db.session.commit()
+    return jsonify({"succes": "Ya se elimino el personaje favorito satisfactoriamente"}), 200  
+
+
+   
+    # favoritosPersonajes= tabla_pivote1.query.all()
+    # favoritosPlanetas= tabla_pivote2.query.all()
+    # print(favoritosPersonajes)
+    # print(favoritosPlanetas)
+    # lista2 = [favoritosPersonajes + favoritosPlanetas]
+    # for favoritos in lista2:
+    #     lista2.append(favoritos.serialize())
+    # return jsonify(lista2), 200   
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,5 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
+from sqlalchemy import Table, Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -7,9 +11,8 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    # def __repr__(self):
+    #     return '<User %r>' % self.username
 
     def serialize(self):
         return {
@@ -17,3 +20,70 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+Base = declarative_base()
+
+tabla_pivote1 = Table('personaje_favorito', db.Model.metadata,
+    Column('usuario_id', ForeignKey('usuario.id'), primary_key=True),
+    Column('personaje_id', ForeignKey('personaje.id'), primary_key=True))
+
+tabla_pivote2 = Table('planeta_favorito', db.Model.metadata,
+    Column('usuario_id', ForeignKey('usuario.id')),
+    Column('planeta_id' , ForeignKey('planeta.id')))
+
+class Usuario(db.Model):
+    __tablename__ = 'usuario'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    contrasena = Column(String(100), nullable=False)
+    personajes = relationship('Personaje', secondary=tabla_pivote1)
+    planetas = relationship('Planeta' , secondary=tabla_pivote2)
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+class Personaje(db.Model):
+    __tablename__ = 'personaje'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    birth_year = Column(String(100), nullable=True)
+    eye_color = Column(String(100), nullable=True)
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "eyeColor": self.eye_color,
+            # do not serialize the password, its a security breach
+        }        
+
+class Planeta(db.Model):
+    __tablename__ = 'planeta'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name_planeta = Column(String(250), nullable=False)
+    gravity = Column(String(100), nullable=True)
+    climate = Column(String(100), nullable=True)
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name_planeta": self.name_planeta,
+            "gravity": self.gravity,
+            "climate": self.climate,
+            # do not serialize the password, its a security breach
+        }
+  
+
+#     def to_dict(self):
+#         return {}
+
+## Draw from SQLAlchemy base
+# render_er(Base, 'diagram.png')
